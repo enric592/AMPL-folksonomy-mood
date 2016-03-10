58128e74c6a8e5c922a11ad6f24ec32d325536d8
Enric <greenagher@gmail.com>
2016-03-10 18:40:41 +0100

    Keeping LastFm CSV out of the repo as it is too big. Adding report.odt.

--- MATLAB/dataSelection.m
+++ MATLAB/dataSelection.m
@@ -8,7 +8,7 @@ its count in that recording (see their documentation). These counts are
 normalised per recording, that is, a count of 100 means that it is the most
 common tag in that recording, not that it has been tagged with this tag 100 times.
  %}
- 
+ tax = ['happy';'sad  '] % Taxonomy
  N = 1000; % Number of desired instances for each class.
  
  %% For each class, we want to get the first N instances where the class tag is more relevant.
@@ -19,5 +19,5 @@ common tag in that recording, not that it has been tagged with this tag 100 time
  % csv file containing two columns, where the first column is a musicbrainz 
  % id, and the second column is the name of your class.
  
- db = cat(1,happyDatabase(:,1:2),sadDatabase(:,1:2));
- cell2csv('../CSV/dataset.csv',db);
+ db = cat(1,happyDatabase(:,1:2),sadDatabase(:,1:2)); % All instances in the same table with their class
+ cell2csv('../CSV/dataset.csv',db);  % Export table into CSV file
